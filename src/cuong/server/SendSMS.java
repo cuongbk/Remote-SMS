839c8a16d7b32207b10cfe80a9f6615a7b1c2b77
Remote SMS <lecuong.fet@gmail.com>
2012-07-26 10:55:28 +0700

    Remote SMS

--- src/cuong/server/SendSMS.java
+++ src/cuong/server/SendSMS.java
@@ -1,57 +1,123 @@
 package cuong.server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import cuong.server.ServerActivity;
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.telephony.gsm.SmsManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-public class SendSMS extends Activity {
-	
-	public String str = "";
+public class ReceiveData extends Activity {
+	private Intent registerReceiver;
 	public String k = "";
 	public String number = "";
 	public String message = "";
+
 	@Override
-	public void onNewIntent(Intent intent1) {
-    Log.d("MapsActivity", "onNewIntent is called!");
-    str = intent1.getStringExtra("KEY").toString();
-    super.onNewIntent(intent1); 
-    char[] arr = str.toCharArray();
-    k += arr[0];
-    int m = Integer.parseInt(k);
-    int n = arr.length;
-    for(int i = 0; i < n; i++)
-    {
-    	if ((i>0) && (i<m+1)) number += arr[i];
-    	if((i>m) && (i<n)) message += arr[i];
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);  
+	try {
+	BufferedReader in = new BufferedReader(new InputStreamReader(ServerActivity.client.getInputStream()));	    
+    String str = null; 
     
-    sendSms(number,message);
-}
+		while ((str = in.readLine()) != null) {                       	
+		     	  Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
+		     	  char[] arr = str.toCharArray();
+		     	    k += arr[0];
+		     	    int m = Integer.parseInt(k);
+		     	    int n = arr.length;
+		     	    for(int i = 0; i < n; i++)
+		     	    {
+		     	    	if ((i>0) && (i<m+1)) number += arr[i];
+		     	    	if((i>m) && (i<n)) message += arr[i];
+		     	    }
+		     	    
+		     	    sendSMS(number,message);
+		}
+		 
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} 
 	
-	//String number = "+841649610768";
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-    }   
-    private void sendSms(String phoneNumber, String message)
-    {
+	}
+	  //---sends an SMS message to another device---
+    private void sendSMS(String phoneNumber, String message)
+    { 
     	
-    	String SENT="SMS_SENT";
-    	String DELIVERED="SMS_DELIVERED";
-    	
-    	PendingIntent pi = PendingIntent.getActivity(this, 0,
-                new Intent(this, SendSMS.class), 0);                
-            SmsManager sms = SmsManager.getDefault();
-            sms.sendTextMessage(phoneNumber, null, message, pi, null);        
-          
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+ 
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+            new Intent(SENT), 0);
+ 
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+            new Intent(DELIVERED), 0);
+ 
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @SuppressWarnings("deprecation")
+			@Override
+            public void onReceive(Context arg0, Intent arg1) {
+            	
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent", 
+                                Toast.LENGTH_SHORT).show();
+                        
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+ 
+        //---when the SMS has been delivered---
+        registerReceiver = registerReceiver(new BroadcastReceiver(){
+			@Override
+			public void onReceive(Context arg0, Intent arg1) {
+				// TODO Auto-generated method stub
+				switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered", 
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered", 
+                                Toast.LENGTH_SHORT).show();
+                        break;                        
+                }
+			}
+        }, new IntentFilter(DELIVERED));        
+ 
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);        
     }
+
 }
